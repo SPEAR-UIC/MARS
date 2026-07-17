70fd12ffdc33d4658e129c257480552e927e7119
yashkurkure <yashkurkure99@gmail.com>
2026-07-17 20:30:02 +0000

    test outputs

--- CLUSTER-26.sh
+++ CLUSTER-26.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run the experiments
-./cqsimcpp ./experiments/exp2acpy.json
-./cqsimcpp ./experiments/exp2acpy.json
+./cqsimcpp ./experiments/exp2a.json
+./cqsimcpp ./experiments/exp2b.json
 
 # Make the plots from CLUSTER'26 submission
 python3 scripts/plot_cluster26.py experiments/exp2a.json experiments/exp2b.json
